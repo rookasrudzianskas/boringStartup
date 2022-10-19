2e6838149e21cf5cb279383061e90636064235bc
Rokas <rookas.rudzenskas@gmail.com>
2022-10-19 20:43:56 +0300

    Rokas

--- src/screens/QuizScreen/QuizScreen.tsx
+++ src/screens/QuizScreen/QuizScreen.tsx
@@ -1,9 +1,19 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import Colors from "../../constants/Colors";
+import {useNavigation} from "@react-navigation/native";
 
 const QuizScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: true,
+            title: 'Quiz'
+        })
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.question}>What is the output?</Text>
@@ -22,9 +32,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.light.white,
+        padding: 10,
     },
     question: {
-
+        fontSize: 20,
+        fontWeight: '500',
     },
     questionImage: {
 
