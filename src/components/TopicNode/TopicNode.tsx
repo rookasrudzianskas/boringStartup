6cd69d672a99a4dce54e67cdfe9ce939c3241a19
Rokas <rookas.rudzenskas@gmail.com>
2022-10-19 14:14:44 +0300

    Rokas

--- src/components/TopicNode/TopicNode.tsx
+++ src/components/TopicNode/TopicNode.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Colors from "../../constants/Colors";
 import {Topic} from "../../types/models";
+import Svg, { Circle, Rect } from 'react-native-svg';
 
 interface TopicNodeProps {
     topic: Topic;
@@ -13,9 +14,23 @@ const TopicNode = ({topic, isDisabled = true}: TopicNodeProps) => {
     return (
         <TouchableOpacity activeOpacity={isDisabled ? 1 : 0.7} style={styles.container}>
             <View style={[styles.progress]}>
-                <View style={[styles.circle, {backgroundColor: isDisabled ? Colors.light.dark : Colors.light.primary}]}>
-                    <Image source={{uri: topic?.icon}} style={styles.image} />
-                </View>
+                <Svg width={'100'} height={'100'} style={styles.progressBar}>
+                    <Circle cx="50" cy="50" r="40" fill="#ddd" />
+                    <Circle
+                        origin="50, 50"
+                        rotation="-90"
+                        cx="50"
+                        cy="50"
+                        r="20"
+                        stroke="#0074d9"
+                        strokeWidth="40"
+                        fill="none"
+                        strokeDasharray="80, 160"
+                    />
+                </Svg>
+                {/*<View style={[styles.circle, {backgroundColor: isDisabled ? Colors.light.dark : Colors.light.primary}]}>*/}
+                {/*    <Image source={{uri: topic?.icon}} style={styles.image} />*/}
+                {/*</View>*/}
             </View>
                 <Text style={styles.title}>{topic?.title || 'Loading...'}</Text>
         </TouchableOpacity>
@@ -36,6 +51,13 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.light.dark,
         padding: 10,
         borderRadius: 999,
+        width: "100%",
+        aspectRatio: 1,
+    },
+    progressBar: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
     },
     circle: {
         width: "100%",
