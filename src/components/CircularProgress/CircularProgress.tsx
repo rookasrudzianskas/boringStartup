40861d3798d39568a8b468bdea8de83bd995edf5
Rokas <rookas.rudzenskas@gmail.com>
2022-10-19 15:16:17 +0300

    Rokas

--- src/components/CircularProgress/CircularProgress.tsx
+++ src/components/CircularProgress/CircularProgress.tsx
@@ -2,27 +2,27 @@
 import React from "react";
 import { View } from "react-native";
 import { Svg, Circle, Text as SVGText } from 'react-native-svg'
+import Colors from "../../constants/Colors";
 
 interface CircularProgressProps {
     size: number;
     strokeWidth: number;
     progress: number;
-    children?: React.ReactNode;
 }
 
 
 const CircularProgress = (props: CircularProgressProps) => {
-    const { size, strokeWidth, text, progress, children } = props;
+    const { size, strokeWidth, progress } = props;
     const radius = (size - strokeWidth) / 2;
     const circum = radius * 2 * Math.PI;
     const svgProgress = (1 - props.progress) * 100;
 
     return (
-        <View style={{margin: 10}}>
+        <View style={{position: 'absolute', top: 0}}>
             <Svg width={size} height={size}>
                 {/* Background Circle */}
                 <Circle
-                    stroke={props.bgColor ? props.bgColor : "#f2f2f2"}
+                    stroke={Colors.light.background}
                     fill="none"
                     cx={size / 2}
                     cy={size / 2}
@@ -31,20 +31,21 @@ const CircularProgress = (props: CircularProgressProps) => {
                 />
 
                 {/* Progress Circle */}
-                <Circle
-                    stroke={props.pgColor ? props.pgColor : "#3b5998"}
-                    fill="none"
-                    cx={size / 2}
-                    cy={size / 2}
-                    r={radius}
-                    strokeDasharray={`${circum} ${circum}`}
-                    strokeDashoffset={radius * Math.PI * 2 * (svgProgress/100)}
-                    strokeLinecap="round"
-                    transform={`rotate(-90, ${size/2}, ${size/2})`}
-                    {...{strokeWidth}}
-                />
+                {progress > 0 && (
+                    <Circle
+                        stroke={Colors.light.secondary}
+                        fill="none"
+                        cx={size / 2}
+                        cy={size / 2}
+                        r={radius}
+                        strokeDasharray={`${circum} ${circum}`}
+                        strokeDashoffset={radius * Math.PI * 2 * (svgProgress/100)}
+                        strokeLinecap="round"
+                        transform={`rotate(-90, ${size/2}, ${size/2})`}
+                        {...{strokeWidth}}
+                    />
+                )}
 
-                {children}
             </Svg>
         </View>
     )
--- src/components/TopicNode/TopicNode.tsx
+++ src/components/TopicNode/TopicNode.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, Image, TouchableOpacity, useWindowDimensions} from 'react-native';
 import Colors from "../../constants/Colors";
 import {Topic} from "../../types/models";
 import Svg, { Circle, Rect } from 'react-native-svg';
@@ -12,14 +12,15 @@ interface TopicNodeProps {
 }
 
 const TopicNode = ({topic, isDisabled = true}: TopicNodeProps) => {
+    const {width} = useWindowDimensions();
+    const  itemsWidth = width / 3 - 30;
     return (
-        <TouchableOpacity activeOpacity={isDisabled ? 1 : 0.7} style={styles.container}>
+        <TouchableOpacity activeOpacity={isDisabled ? 1 : 0.7} style={[styles.container, {width: itemsWidth}]}>
             <View style={[styles.progress]}>
-                <CircularProgress size={120} strokeWidth={10} progress={topic.progress}>
-                    <View style={[styles.circle, {backgroundColor: isDisabled ? Colors.light.dark : Colors.light.primary}]}>
+                <CircularProgress size={itemsWidth} strokeWidth={8} progress={topic.progress} />
+                    <View style={[styles.circle, {width: itemsWidth - 20, backgroundColor: isDisabled ? Colors.light.dark : Colors.light.primary}]}>
                         <Image source={{uri: topic?.icon}} style={styles.image} />
                     </View>
-                </CircularProgress>
             </View>
                 <Text style={styles.title}>{topic?.title || 'Loading...'}</Text>
         </TouchableOpacity>
@@ -33,30 +34,21 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         margin: 10, // Find the right one @TODO
-        width: "30%",
         maxWidth: 150,
     },
     progress: {
-        backgroundColor: Colors.light.dark,
-        padding: 10,
-        borderRadius: 999,
         width: "100%",
         aspectRatio: 1,
-    },
-    progressBar: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     circle: {
-        width: "70%",
+        alignSelf: 'center',
         aspectRatio: 1,
         borderRadius: 999,
         backgroundColor: Colors.light.tertiary,
         justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 3,
-        borderColor: Colors.light.background,
+        alignItems: 'center'
     },
     image: {
         width: "50%",
