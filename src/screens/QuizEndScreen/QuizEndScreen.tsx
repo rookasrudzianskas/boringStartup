a57db2447ae2954a68bf71bfe540ba29407d60aa
Rokas <rookas.rudzenskas@gmail.com>
2022-10-20 15:02:04 +0300

    Rokas

--- src/screens/QuizEndScreen/QuizEndScreen.tsx
+++ src/screens/QuizEndScreen/QuizEndScreen.tsx
@@ -5,38 +5,32 @@ import {RootStackScreenProps} from "../../types/navigation";
 import LottieView from 'lottie-react-native';
 
 const QuizEndScreen = ({route}: RootStackScreenProps<"QuizEndScreen">) => {
-    const {nOfQuestions, nOfCorrectAnswers} = route.params;
+    const {nOfQuestions, nOfCorrectAnswers} = route?.params;
     const animation = useRef(null);
+
     useEffect(() => {
         // You can control the ref programmatically, rather than using autoPlay
         // animation.current?.play();
-        animation.current?.play();
+        // animation.current?.play();
     }, []);
 
     return (
-        <View style={styles.animationContainer}>
+        <View style={styles.animationContainer} className="justify-center items-center">
             <LottieView
                 autoPlay
+                loop
+                speed={1}
                 ref={animation}
                 style={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: '#eee',
+                    width: 400,
+                    height: 400,
+                    backgroundColor: 'whit',
                 }}
                 // Find more Lottie files at https://lottiefiles.com/featured
-                source={'https://assets4.lottiefiles.com/packages/lf20_dXNLiFDtBY.json'}
+                source="https://assets4.lottiefiles.com/packages/lf20_dXNLiFDtBY.json"
             />
-            <View style={styles.buttonContainer}>
-                <Button
-                    title="Restart Animation"
-                    onPress={() => {
-                        animation.current?.reset();
-                        animation.current?.play();
-                    }}
-                />
-            </View>
-            <Text>{(nOfCorrectAnswers / nOfQuestions) * 100}%</Text>
-            <Text>{nOfCorrectAnswers} / {nOfQuestions}</Text>
+            <Text className="text-4xl font-bold tracking-wider">{(nOfCorrectAnswers / nOfQuestions) * 100 || 'Loading...'}%</Text>
+            <Text className="text-xl font-bold mt-2">{nOfCorrectAnswers} / {nOfQuestions || 'Loading..'}</Text>
         </View>
     );
 };
