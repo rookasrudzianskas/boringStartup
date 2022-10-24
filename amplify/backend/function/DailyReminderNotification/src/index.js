91a0c7f6218627ab4e6af898883e3f70ede18eb6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-24 14:48:54 +0300

    Rokas

--- amplify/backend/function/DailyReminderNotification/src/index.js
+++ amplify/backend/function/DailyReminderNotification/src/index.js
@@ -7,24 +7,43 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const AWS = require('aws-sdk');
 
 const {USER_TABLE} = process.env;
 
+const docClient = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
 
     // get All users from the database
+    const params = {
+        TableName : USER_TABLE,
+    };
 
+    const users = await docClient.scan(params).promise();
+    console.log('Sending notification to:', users.Items.length(), "users", JSON.stringify(users));
     // send notification to all users
-
+    await Promise.all(users.Items.map(user => user?.expoNotificationToken && sendPushNotification(user.expoNotificationToken)));
     // later personalize the message based on the user progress. @TODO
 
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    return "Finished";
 };
+
+export async function sendPushNotification(expoPushToken) {
+    const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Original Title',
+        body: 'And here is the body!',
+        data: { someData: 'goes here' },
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Accept-encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    });
+}
