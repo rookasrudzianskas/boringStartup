66f41683241ba85e82debad716f72a514d113f15
Rokas <rookas.rudzenskas@gmail.com>
2022-10-25 12:39:47 +0300

    Rokas

--- src/screens/Paywall/Paywall.tsx
+++ src/screens/Paywall/Paywall.tsx
@@ -1,8 +1,25 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {adapty, AdaptyError, AdaptyPaywall} from "react-native-adapty";
 
 const Paywall = () => {
+    const [paywall, setPaywall] = useState<AdaptyPaywall | null>([]);
+    useEffect(() => {
+        const fetchPaywall = async () => {
+            try {
+                const {paywalls, products} = await adapty.paywalls.getPaywalls({ forceUpdate: true });
+                if(paywall.length > 0) {
+                    setPaywall(paywalls[0]);
+                }
+            } catch (error: AdaptyError) {
+                // console.log("Error", error);
+            }
+        }
+
+        fetchPaywall();
+    }, []);
+
     return (
         <View>
             <Text>
