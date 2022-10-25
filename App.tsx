47aa76e2a63d5a2c4f54ef3190a7c99534adc6da
Rokas <rookas.rudzenskas@gmail.com>
2022-10-25 09:25:56 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -27,7 +27,6 @@ const App = () => {
     const isLoadingComplete = useCachedResources();
     const colorScheme = useColorScheme();
 
-
     if (!isLoadingComplete) {
         return null;
     } else {
--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,5 @@
-const { makeMetroConfig } = require("@rnx-kit/metro-config");
-const {
-    DuplicateDependencies,
-} = require("@rnx-kit/metro-plugin-duplicates-checker");
-const { MetroSerializer } = require("@rnx-kit/metro-serializer");
-
-module.exports = makeMetroConfig({
+module.exports = {
     resolver: {
         blacklistRE: /#current-cloud-backend\/.*/
-    },
-    projectRoot: __dirname,
-    serializer: {
-        customSerializer: MetroSerializer([DuplicateDependencies()]),
     }
-});
-
+};
